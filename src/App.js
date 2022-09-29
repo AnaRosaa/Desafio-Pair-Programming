71894668424a7d4337f7395c899b70a6995593a3
MichelleConceição <liliandlentretenimento@gmail.com>
2022-09-28 22:18:58 -0300

    Desafio Finalizado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { GlobalStyle } from "./Global/style.js";
 import Header from "./Components/Header/header.jsx";
 import Main from "./Components/Main/main.jsx";
 import MainTwo from "./Components/MainTwo/maintwo.jsx"
@@ -8,7 +7,6 @@ import Footer from "./Components/Footer/footer.jsx";
 export default function App() {
   return (
     <>
-      <GlobalStyle />
       <Header />
       <Main />
       <MainTwo />
